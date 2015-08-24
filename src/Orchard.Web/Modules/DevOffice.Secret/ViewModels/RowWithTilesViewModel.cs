bf4aaae7a18b1bf8c52632fd03b8a622cf46c425
rachelAdams <rachel.adams1012@gmail.com>
2015-08-24 09:37:30 -0700

    adding rows with tiles page files back into

---
+++ src/Orchard.Web/Modules/DevOffice.Secret/ViewModels/RowWithTilesViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DevOffice.Secret.Models;
+
+namespace DevOffice.Secret.ViewModels
+{
+    public class RowWithTilesViewModel
+    {
+        public RowWithTilesViewModel()
+        {
+            Rows = new List<SingleRowWithTilesRecord>();
+        }
+
+        public IList<SingleRowWithTilesRecord> Rows { get; set; }
+        public string TilesJson { get; set; }
+    }
+
+}
